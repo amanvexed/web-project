ed5c85a9b80d29c1ef6ba44ccb5ea91d9414ad2e
osboxes <osboxes@example.com>
2021-09-24 11:12:41 +0100

    commit 24092021 1112

--- app/Http/Controllers/VTPassController.php
+++ app/Http/Controllers/VTPassController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Helpers\VTHost;
 use Exception;
+use Unicodeveloper\Paystack\Paystack;
 
 class VTPassController extends Controller
 {
@@ -33,9 +34,26 @@ class VTPassController extends Controller
 
     public function processService(Request $request){
 
-           print("1");
-           print(request()->serviceID);
+           $trxref = $request->input('trxref');
+           $refer = $request->input('reference');
+
+           //check transaction was successfule
+           $paystack = new Paystack();
+           
+
+           //Log success into VTPass and Paystack DB
+           //$this->updateVTPass($trxref,"VTPASS_PAYSTACK_PAYMENT_DONE");
+           //$request['refer']=$refer;
+           //$this->insertIntoPaystack($request);
     }
 
+    public function insertIntoPaystack(Request $request){
+        $paystacklogscontroller = new PaystackLogsController();
+        $paystacklogscontroller->store($request);
+    }
 
+    public function updateVTPass($transactionId, $text){
+        $vtpasslogscontroller = new VTPassLogsController();
+        $vtpasslogscontroller->updateRecord($transactionId,$text);
+    }
 }
