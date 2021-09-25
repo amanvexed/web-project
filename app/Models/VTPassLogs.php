f26edebac2050c0178e56ee7a317c7efa9e050a9
osboxes <osboxes@example.com>
2021-09-25 14:32:39 +0100

    commit 25092021 1432

--- app/Models/VTPassLogs.php
+++ app/Models/VTPassLogs.php
@@ -13,12 +13,30 @@ class VTPassLogs extends Model
 
 
     protected $fillable = [
+        'vtpass_status',
+        'product_name',
+        'unique_element',
+        'unit_price',
+        'quantity',
+        'service_verification',
+        'channel',
+        'commission',
+        'total_amount',
+        'discount',
+        'type',
         'email',
+        'phone',
+        'name',
+        'convinience_fee',
+        'amount',
+        'platform',
+        'method',
         'transactionid',
         'serviceid',
-        'product',
-        'mobilenumber',
-        'paystack_reference',
-        'amount'
+        'variationcode',
+        'response_description',
+        'transaction_date',
+        'status',
+        'full_response'
     ];
 }
